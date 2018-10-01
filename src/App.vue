cc4bdf6f37534e49175b2dcfb7bfc055946996b6
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-10-01 07:33:15 +0700

    Interactive map with here

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HereMap/>
+  </div>
+</template>
+
+<script>
+import HereMap from './components/HereMap.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HereMap
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HereMap.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="here-map" id="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'HereMap',
+  mounted() {
+    const platform = new H.service.Platform({
+      app_id: 'FymdsOx1OInDIkWIBErG',
+      app_code: 'i_LyBjYPh3K-ymfzPrpBPw'
+    });
+
+    const defaultLayers = platform.createDefaultLayers();
+
+    const map = new H.Map(document.getElementById('map'), defaultLayers.normal.map, {
+      zoom: 10,
+      center: { lng: 106.660172, lat: 10.762622 }
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.here-map {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>

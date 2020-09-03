8b4c3ed93a0fac05ec0aa3d672cc1a7ad7708f08
Dang Van Thanh <dangvanthanh@dangthanh.org>
2020-09-04 05:50:19 +0700

    Migration 5x to 6x storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions/register',
+    '@storybook/addon-links/register',
+    '@storybook/addon-options/register',
+  ],
+};
